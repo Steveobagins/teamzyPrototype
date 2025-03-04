5177c2a32f4edc3f083e42161883d2bf989ab6a4
Steve <steveobagins@gmail.com>
2025-03-04 00:12:17 +0000

    Version 2 - 040325-00:12

---
+++ scripts/components/errorMessage.js
@@ -0,0 +1,7 @@
+// scripts/components/errorMessage.js
+
+export function createErrorMessage(message) {
+    return `<div class="error-message" style="color: var(--color-error); margin-top: var(--space-xs);">${message}</div>`;
+}
+
+// End of code
